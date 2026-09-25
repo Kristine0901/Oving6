462f072b8f909eef6594a142a12a038de9011235
Kristine0901 <0901ksol0901@gmail.com>
2026-09-25 13:34:37 +0200

    Endring av sigma

--- oving6.py
+++ oving6.py
@@ -18,4 +18,17 @@ plt.plot(tidsakse,G(tidsakse, A, my, sigma))
 plt.xlabel("Tid [timer]")
 plt.ylabel("Innstråling [W/m²]")
 plt.grid()
+plt.savefig("Ovinger/Oving6/SolinnstralingEnSigma.png")
+
+plt.show()
+
+#Hvordan påvirker sigma modellen? 
+
+plt.plot(tidsakse,G(tidsakse, A, my, 3))
+plt.plot(tidsakse,G(tidsakse, A, my, 5))
+plt.plot(tidsakse,G(tidsakse, A, my, 1))
+plt.xlabel("Tid [timer]")
+plt.ylabel("Innstråling [W/m²]")
+plt.grid()
+plt.savefig("Ovinger/Oving6/SolinnstralingTreSigmaer.png")
 plt.show()
