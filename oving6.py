09bc21ebd5fdf713e7e06dd24f0da88b4043bf55
Kristine0901 <0901ksol0901@gmail.com>
2026-09-25 13:29:27 +0200

    Oppgave 1

---
+++ oving6.py
@@ -0,0 +1,21 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+def G(t,A, my, sigma):
+    solinnstraling = A * np.exp(-(t-my)**2 / (2*sigma**2))
+    return solinnstraling
+
+
+
+tidsakse = np.linspace(0, 23, 23)
+
+A = 800
+my = 13
+
+sigma = 3
+
+plt.plot(tidsakse,G(tidsakse, A, my, sigma))
+plt.xlabel("Tid [timer]")
+plt.ylabel("Innstråling [W/m²]")
+plt.grid()
+plt.show()
